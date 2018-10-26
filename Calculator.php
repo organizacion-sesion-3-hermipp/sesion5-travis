594eed69bf199220c64d131bf96f65b09f1f36a7
Herminia Pastor Pina <hermipp@yahoo.es>
2018-10-26 13:13:08 -0700

    P5 ficheros iniciales

---
+++ Calculator.php
@@ -0,0 +1,20 @@
+<?php
+class Calculator
+{
+ 
+    public function add($a, $b)
+    {
+        return $a + $b;
+    }
+
+    public function multiply($a, $b)
+    {
+        return $a * $b;
+    }
+
+    public function division($a, $b)
+    {
+        return $a / $b;
+    }
+ 
+}
